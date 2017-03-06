d877564b5d3ef8001bb16794a87bebccd30d464a
Ted Won <iamtedwon@gmail.com>
2017-03-06 17:09:46 +0900

    My PR 201703061722

--- app/src/main/java/org/jbugkorea/app/Main.java
+++ app/src/main/java/org/jbugkorea/app/Main.java
@@ -4,6 +4,8 @@ import org.jbugkorea.hello.Hello;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("My PR 201703061709");
+        System.out.println("My PR 201703061722");
         new Hello().say(); // PR test
     }
 }
