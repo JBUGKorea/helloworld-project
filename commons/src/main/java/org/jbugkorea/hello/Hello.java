ea1e3a78ba4d7098066953ae51d773905e708c06
Ted Won <iamtedwon@gmail.com>
2017-03-06 17:12:51 +0900

    My rebase/squash test

--- commons/src/main/java/org/jbugkorea/hello/Hello.java
+++ commons/src/main/java/org/jbugkorea/hello/Hello.java
@@ -3,5 +3,12 @@ package org.jbugkorea.hello;
 public class Hello {
     public void say() {
         System.out.println("Hello!");
+        System.out.println("my code change #1");
+        System.out.println("my code change #2");
+        System.out.println("my code change #3");
+        System.out.println("my code change #4");
+        System.out.println("my code change #5");
+        System.out.println("my code change #6");
+        System.out.println("my code change #7");
     }
 }
