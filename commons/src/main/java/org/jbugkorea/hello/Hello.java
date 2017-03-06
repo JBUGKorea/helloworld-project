ee1e85ac743e2fbcec2820c79822371da5a54473
Ted Won <iamtedwon@gmail.com>
2017-03-06 17:14:12 +0900

    my code change #4

--- commons/src/main/java/org/jbugkorea/hello/Hello.java
+++ commons/src/main/java/org/jbugkorea/hello/Hello.java
@@ -6,5 +6,6 @@ public class Hello {
         System.out.println("my code change #1");
         System.out.println("my code change #2");
         System.out.println("my code change #3");
+        System.out.println("my code change #4");
     }
 }
